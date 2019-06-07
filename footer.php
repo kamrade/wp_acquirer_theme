ecdbbaa2cb93b5aa9a96eeab3598e0725f68bf61
Denis Michailov <kamrad.reg@gmail.com>
2019-06-07 23:47:46 +1000

    Customize navigation. Add widgets

--- footer.php
+++ footer.php
@@ -2,7 +2,11 @@
   <footer class="site-footer">
     <div class="container">
 
-    <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
+    <p>
+      <?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?>
+    </p>
+
+    <p>© 2009-2019 CardPay. All rights reserved. CardPay, the logo and any associated brand names are CardPay trademarks. All other trademarks are property of their respective owners.</p>
 
     <nav class="site-nav-footer">
       <?php $args = array( 'theme_location' => 'footer' ); ?>
