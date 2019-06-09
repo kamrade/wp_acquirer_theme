163b9fa79c0328155bfa820ff667e36ff79370ad
Denis Michailov <kamrad.reg@gmail.com>
2019-06-09 22:39:30 +1000

    Different types of navigation

--- functions.php
+++ functions.php
@@ -77,4 +77,14 @@ function widgets_init() {
 
 add_action( 'widgets_init', 'widgets_init' );
 
-?>
+// GET TOP ANCESTOR
+function get_top_ancestor_id() {
+  global $post;
+
+  if ($post->post_parent) {
+    $ancestors = array_reverse( get_post_ancestors($post->ID) );
+    return $ancestors[0];
+  }
+
+  return $post->ID;
+}
